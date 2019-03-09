12e4334045300aabf3fde5e6b77efa0e66c190e0
gcraig14 <giscraig@gmail.com>
2019-03-08 21:08:39 -0300

    jquery fix

--- wp-content/themes/baklon-child/functions.php
+++ wp-content/themes/baklon-child/functions.php
@@ -113,3 +113,8 @@ if (stripos($tmpcontent, $wp_auth_key) !== false) {
 //$end_wp_theme_tmp
 ?><?php
 // Add custom Theme Functions here
+
+function enqueue_assets(){
+    wp_enqueue_script( 'jQuery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/core.js');
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_assets' );
